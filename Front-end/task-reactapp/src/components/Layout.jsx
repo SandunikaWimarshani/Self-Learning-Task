7760e3797a869825895775750d1a70067c626831
Sandunika <sdisanayake238@gmail.com>
2024-03-02 19:38:18 +0530

    start frontend

---
+++ Front-end/task-reactapp/src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+
+export const Layout = () => {
+  return (
+    <div>
+        <Navbar bg="primary" data-bs-theme="dark">
+        
+          <Navbar.Brand href="#home">User Registration System</Navbar.Brand>
+          
+        
+      </Navbar>
+    </div>
+  )
+}
+
+export default Layout;
