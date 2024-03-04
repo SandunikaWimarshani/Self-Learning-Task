b63114367ade2534054eca0fcc6890c3fab2fbeb
Sandunika <sdisanayake238@gmail.com>
2024-03-04 09:32:27 +0530

    modify nestjs controller

--- Back-end/src/User/user.controller.ts
+++ Back-end/src/User/user.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Put} from "@nestjs/common";
 import { UserService } from "./user.service";
+import { User } from "./schema/user-schema";
 
 @Controller('user')
 
@@ -12,5 +13,22 @@ export class UserController{
     async getAll(){
         return await this.userService.getAll();
         
+    }
+
+    @Post()
+    async create(@Body() user:User){
+        return await this.userService.create(user);
+    }
+
+    @Get("/:id")
+    async getById(@Param('id') id:string){
+        return await this.userService.getById(id);
+
+    }
+
+    @Put("/:id")
+    async update(@Param('id') id:string, @Body() user:User){
+        return await this.userService.update(id, user);
+
     }
 }
--- Back-end/src/User/user.service.ts
+++ Back-end/src/User/user.service.ts
@@ -11,4 +11,20 @@ export class UserService {
     async getAll(){
         return await this.userModel.find().exec();
     }
+
+    async create(user:User){
+        const newUser = new this.userModel(user);
+        await newUser.save();
+    }
+
+    async getById(id:string){
+        return await this.userModel.findById(id).exec();
+
+    }
+
+    async update(id:string, user:User){
+        return await this.userModel.findByIdAndUpdate(id, user,{
+            new:true
+        });
+    }
 }
